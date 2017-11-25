d43c1ca5b91f275fb89343a174ac99c4eb88aede
focusheart <focusheart@qq.com>
2017-11-25 19:54:40 +0800

    update lots of things

---
+++ static/js/color.schema.black.js
@@ -0,0 +1,15 @@
+var colorschema = {
+    bgColor: '#000000',
+    titleTextColor: '#FFFFFF',
+    provAreaColor: '#000000',
+    provBorderColor: 'rgba(100,149,237,0.7)',
+    provLabelColor: 'rgba(200,200,237,0.2)',
+    crossLineColor: 'rgba(204, 246, 255, 0.2)',
+    crossPointColor: 'rgba(204, 246, 255, 0.2)',
+    lcTriBorderColor: 'rgba(255, 0, 0, 0.5)',
+    fixLineColor: 'rgba(2, 166, 253, 0.5)',
+    popRangeColors: ['red','orange','yellow','lime','aqua'],
+    popRangeTextColor: '#FFFFFF',
+    provBorderWidth: 1,
+    crossLineWidth: 0.5
+};
---
+++ static/js/color.schema.white.js
@@ -0,0 +1,15 @@
+var colorschema = {
+    bgColor: '#FFFFFF',
+    titleTextColor: 'black',
+    provAreaColor: '#FFFFFF',
+    provBorderColor: 'rgba(0,1,0,0.9)',
+    provLabelColor: 'rgba(200,200,237,0.2)',
+    crossLineColor: 'rgba(2, 166, 253, 1)',
+    crossPointColor: 'rgba(0, 0, 255, 0.5)',
+    lcTriBorderColor: 'rgba(255, 0, 0, 0.9)',
+    fixLineColor: 'rgba(2, 166, 253, 0.5)',
+    popRangeColors: ['red','orange','yellow','lime','blue'],
+    popRangeTextColor: '#000000',
+    provBorderWidth: 3,
+    crossLineWidth: 2
+};
